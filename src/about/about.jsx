76412c8c65b7628a79a62594534ce4f2480cce37
Luiz Ricardo Vicente Rita <luiz.rita@amcom.com.br>
2018-04-20 14:58:27 -0300

    Criando componentes para melhorar layout

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' ></PageHeader>
+        <h2>Minha História</h2>
+        <p>Olá sou o Luiz codificador java aprendendo React</p>
+        <h2>Objetivo</h2>
+        <p>Iniciar meu conhecimento em React e evoluir no mundo web</p>
     </div>
 )
